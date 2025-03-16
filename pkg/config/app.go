1954cd5f32cbba75b852320714785ea8e3eeca68
Gabriel_devs <dcbiel846@gmail.com>
2025-03-16 15:40:05 -0300

    first commit

---
+++ pkg/config/app.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func ConnectDB() {
+	dsn := os.Getenv("DB_DSN")
+	d, err := gorm.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
