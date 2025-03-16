3ffc0359d607b55ab502e41b5dedb8c616c6f8e1
Gabriel_devs <dcbiel846@gmail.com>
2025-03-16 16:01:06 -0300

    Implement main function to set up HTTP server and register routes

---
+++ cmd/main/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/GabrielNat1/BookStore/pkg/routes"
+	"github.com/goriila/mux"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookstoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:8080", r|nil))
+}
